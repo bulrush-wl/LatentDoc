28c57983f7ee5458c6d044454eaf25a9c67f1cb4
lw <wlu22@m.fudan.edu.cn>
2024-08-18 13:13:33 +0000

    fix bug

--- latentdoc/model/sam_opt_1024_with_ae_with_projector_down4_recon.py
+++ latentdoc/model/sam_opt_1024_with_ae_with_projector_down4_recon.py
@@ -364,7 +364,7 @@ class LatentDocOPTForCausalLM(OPTForCausalLM):
             GPP_criterion=GradientPriorLoss()
             loss_fct = CrossEntropyLoss()
             ae_loss = loss_fct(shift_logits.view(-1, self.config.vocab_size), shift_labels.view(-1))
-            print('self.with_ae_loss',self.with_ae_loss)
+            print('self.with_ae_loss',self.config.mm_cfg.with_ae_loss)
             if self.config.mm_cfg.with_ae_loss==True:
                 # print('celoss=',loss)
                 l2_loss=criterion(recon,raw_images)
